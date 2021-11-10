f6a37524c5f938e2029b17fc702ed11da60eff9b
Yaaseen Davids <yaaseendav@gmail.com>
2021-11-10 14:40:28 +0200

    lots

---
+++ javascript/contact.js
@@ -0,0 +1,15 @@
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+  new WOW().init();
+
+
+  	/* AUTHOR LINK */
+     $('.about-me-img').hover(function(){
+            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+        }, function(){
+            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+        });
