0ae6a2debe9ed962560e8f8295a1139aa2cc6645
Yaaseen Davids <yaaseendav@gmail.com>
2021-11-11 16:11:34 +0200

    heading

--- javascript/contact.js
+++ javascript/contact.js
@@ -13,3 +13,23 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
         }, function(){
             $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
         });
+        (function($) {
+            var s,
+            spanizeLetters = {
+              settings: {
+                letters: $('.js-spanize'),
+              },
+              init: function() {
+                s = this.settings;
+                this.bindEvents();
+              },
+              bindEvents: function(){
+                s.letters.html(function (i, el) {
+                  //spanizeLetters.joinChars();
+                  var spanizer = $.trim(el).split("");
+                  return '<span>' + spanizer.join('</span><span>') + '</span>';
+                });
+              },
+            };
+            spanizeLetters.init();
+          })(jQuery);
